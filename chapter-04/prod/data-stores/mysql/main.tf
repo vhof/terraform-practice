0bd4cf9f46be8d2f840f8ab0c9f858b79e0b02ee
vhof <v-vant_hof@live.nl>
2026-03-17 12:42:14 +0100

    chapter-04 swap to versioned 'remote' modules

--- chapter-04/prod/data-stores/mysql/main.tf
+++ chapter-04/prod/data-stores/mysql/main.tf
@@ -11,7 +11,17 @@ locals {
 }
 
 module "mysql" {
-  source  = "../../../modules/data-stores/mysql"
+  # Terraform supports retrieving modules from remote sources. 
+  # Conventionally, our modules should live in a seperate repository,
+  # but I didn't feel like doing that. So, this 'remote' source is actually
+  # this same repository.
+  # Terraform performs a git clone operation to retrieve thise remote source,
+  # so we use depth=1 to prevent unnecessary commit history cloning. 
+  # We use tags in our repository to mark module versions, and select these versions
+  # using the ref parameter. This way, we can test changes to the modules in our 
+  # staging environment by selecting a different version, without affecting our
+  # production environment. 
+  source  = "github.com/vhof/terraform-practice//chapter-04/modules/data-stores/mysql?depth=1&ref=v0.0.1"
   stadium = local.stadium
   
   instance_type = "db.t3.micro"
--- chapter-04/prod/services/webserver-cluster/main.tf
+++ chapter-04/prod/services/webserver-cluster/main.tf
@@ -14,11 +14,22 @@ locals {
 }
 
 module "webserver_cluster" {
-  source  = "../../../modules/services/webserver-cluster"
+  # Terraform supports retrieving modules from remote sources. 
+  # Conventionally, our modules should live in a seperate repository,
+  # but I didn't feel like doing that. So, this 'remote' source is actually
+  # this same repository.
+  # Terraform performs a git clone operation to retrieve thise remote source,
+  # so we use depth=1 to prevent unnecessary commit history cloning. 
+  # We use tags in our repository to mark module versions, and select these versions
+  # using the ref parameter. This way, we can test changes to the modules in our 
+  # staging environment by selecting a different version, without affecting our
+  # production environment. 
+  source  = "github.com/vhof/terraform-practice//chapter-04/modules/services/webserver-cluster?depth=1&ref=v0.0.1"
   bucket  = var.bucket
   stadium = local.stadium
 
-  # REPLACED BY stadium
+  # ALTERED: following input variables are replaced by stadium, and values are
+  # constructed inside of the modules
   # cluster_name        = "webserver-prod"
   # db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
 
