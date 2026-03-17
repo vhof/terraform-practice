58b7ed0b88d2b274e8d1d3d31126b49b95bc9092
vhof <v-vant_hof@live.nl>
2026-03-17 12:57:49 +0100

    chapter-04 move documentation

--- chapter-04/stage/services/webserver-cluster/main.tf
+++ chapter-04/stage/services/webserver-cluster/main.tf
@@ -14,16 +14,6 @@ locals {
 }
 
 module "webserver_cluster" {
-  # Terraform supports retrieving modules from remote sources. 
-  # Conventionally, our modules should live in a seperate repository,
-  # but I didn't feel like doing that. So, this 'remote' source is actually
-  # this same repository.
-  # Terraform performs a git clone operation to retrieve this remote source,
-  # so we use depth=1 to prevent unnecessary commit history cloning. 
-  # We use tags in our repository to mark module versions, and select these versions
-  # using the ref parameter. This way, we can test changes to the modules in our 
-  # staging environment by selecting a different version, without affecting our
-  # production environment. 
   source  = "github.com/vhof/terraform-practice//chapter-04/modules/services/webserver-cluster?depth=1&ref=v0.0.1"
   bucket  = var.bucket
   stadium = local.stadium
